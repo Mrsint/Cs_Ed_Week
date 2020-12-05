d13cfcbeb3ada291db7ad84f4da8c3283f21510c
Shane Sint <ssint@schools.nyc.gov>
2020-12-05 15:54:50 +0000

    First update: state

--- src/App.js
+++ src/App.js
@@ -5,20 +5,39 @@ import Navbar from'./Components/Navbar';
 import Dashboard from'./Components/Dashboard';
 
 class App extends React.Component{
+      constructor(props){
+        super(props)
+        this.state = {
+            "game": "Please select a game",
+            "productivity": "Please select a productivity app",
+            "story": "Please select a story",
+            "social": "Please select a social app",
+            "study:": "Please select a study skills app"
+
+        }
+    }
+
+    makeChoice = (type, choice) =>{
+        this.setState(state => {
+            state[type]= choice
+            return state
+        } )
+    }
 render(){
     return (
       <div className="App">
           
           <Navbar/>
-          <Dashboard/>
+          <Dashboard game= {this.state.game} productivity={this.state.productivity} story={this.state.story} social= {this.state.social} studySkill= {this.state.studySkill}/>
           <div className="card-container">
-            <Card activity = "Game" />
-            <Card activity = "Story"/>
-            <Card activity = "Study Skills"/>
-            <Card activity = "Productvity"/>
-            <Card activity = "Social"/>/>
+            <Card type = "game" title= "flappy bird" makeChoice= {this.makeChoice}/>
+            <Card type = "story" title= "zelda" makeChoice= {this.makeChoice}/>
+            <Card type = "productivity" title= "reminders" makeChoice= {this.makeChoice}/>
+            <Card type = "study" title= "note taking" makeChoice= {this.makeChoice}/>
+            <Card type = "social" title= "facebook" makeChoice= {this.makeChoice}/>
+           </div>
         </div>
-    </div>
+        
   );
 }
 }
--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-function Dashboard() {
+function Dashboard(props) {
   return (
     <div className="Dashboard">
 
         <h1>Available Activities</h1>
         <div className ="apps">
-          <p className="app"> Games: </p>
-          <p className="app"> Story:  </p>
-          <p className="app"> Prodocutivity:  </p>
-          <p className="app"> Study Skills: </p>
-          <p className="app"> Social: </p>
+          <p className="app"> Games: {props.game}  </p>
+          <p className="app"> Story:  {props.story} </p>
+          <p className="app"> Prodocutivity:{props.productivity}  </p>
+          <p className="app"> Study Skills:{props.study} </p>
+          <p className="app"> Social: {props.social}</p>
         </div>
       </div>
   );
